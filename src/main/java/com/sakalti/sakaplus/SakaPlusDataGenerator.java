ab09f46dc7da59be034e00b35d2464f1871b4424
Konnitiwa768 <konnitiwaa1@outlook.jp>
2025-06-03 13:15:56 +0900

    Update SakaPlusDataGenerator.java

--- src/main/java/com/sakalti/sakaplus/SakaPlusDataGenerator.java
+++ src/main/java/com/sakalti/sakaplus/SakaPlusDataGenerator.java
@@ -2,7 +2,6 @@ package com.sakalti.sakaplus;
 
 import com.sakalti.sakaplus.datagen.SakaModelProvider;
 import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
-import net.fabricmc.fabric.api.datagen.v1.provider.FabricDataProvider;
 
 public class SakaPlusDataGenerator {
     public static void gatherData(FabricDataGenerator generator) {
