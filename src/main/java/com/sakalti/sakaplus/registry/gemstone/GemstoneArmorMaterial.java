52edc86d6834781ba720525c792dd3972959fd20
Konnitiwa768 <konnitiwaa1@outlook.jp>
2025-06-04 08:32:32 +0900

    Create GemstoneArmorMaterial.java

---
+++ src/main/java/com/sakalti/sakaplus/registry/gemstone/GemstoneArmorMaterial.java
@@ -0,0 +1,53 @@
+package com.sakalti.sakaplus.registry.gemstone;
+
+import net.minecraft.item.ArmorMaterial;
+import net.minecraft.item.EquipmentSlot;
+import net.minecraft.item.Items;
+import net.minecraft.sound.SoundEvent;
+import net.minecraft.sound.SoundEvents;
+import net.minecraft.recipe.Ingredient;
+
+public class GemstoneArmorMaterial implements ArmorMaterial {
+    private static final int[] BASE_DURABILITY = {13, 15, 16, 11};
+    private static final int[] PROTECTION = {2, 6, 5, 2};
+
+    @Override
+    public int getDurability(EquipmentSlot slot) {
+        return BASE_DURABILITY[slot.getEntitySlotId()] * 31;
+    }
+
+    @Override
+    public int getProtectionAmount(EquipmentSlot slot) {
+        return PROTECTION[slot.getEntitySlotId()];
+    }
+
+    @Override
+    public int getEnchantability() {
+        return 20;
+    }
+
+    @Override
+    public SoundEvent getEquipSound() {
+        return SoundEvents.ITEM_ARMOR_EQUIP_DIAMOND;
+    }
+
+    @Override
+    public Ingredient getRepairIngredient() {
+        return Ingredient.ofItems(Items.LAPIS_LAZULI, Items.DIAMOND);
+    }
+
+    @Override
+    public String getName() {
+        return "gemstone";
+    }
+
+    @Override
+    public float getToughness() {
+        return 1.0f;
+    }
+
+    @Override
+    public float getKnockbackResistance() {
+        return 0.0f;
+    }
+}
