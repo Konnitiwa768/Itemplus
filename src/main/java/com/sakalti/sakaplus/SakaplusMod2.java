737fd36d4c20f3893d35916b6ad2e3ed941a0bf9
Konnitiwa768 <konnitiwaa1@outlook.jp>
2025-06-08 20:54:38 +0900

    Create SakaplusMod2.java

---
+++ src/main/java/com/sakalti/sakaplus/SakaplusMod2.java
@@ -0,0 +1,10 @@
+package com.sakalti.sakaplus;
+
+import net.fabricmc.api.ModInitializer;
+import com.sakalti.sakaplus.block.ModBlocks2.java
+public class SakaplusMod2 implements ModInitializer {
+    @Override
+    public void onInitialize() {
+        ModBlocks2.registerBlocks();
+    }
+}
