b231f2a7a2fa799254ab23fdd52dc2af84689bec
Konnitiwa768 <konnitiwaa1@outlook.jp>
2025-06-09 17:06:14 +0900

    Create ModEntities.java

---
+++ src/main/java/com/sakalti/sakaplus/registry/ModEntities.java
@@ -0,0 +1,33 @@
+package com.sakalti.sakaplus.registry;
+
+import com.sakalti.sakaplus.entity.ScorcherEntity;
+import net.fabricmc.fabric.api.biome.v1.BiomeModifications;
+import net.fabricmc.fabric.api.biome.v1.BiomeSelectors;
+import net.fabricmc.fabric.api.object.builder.v1.entity.FabricDefaultAttributeRegistry;
+import net.minecraft.entity.EntityDimensions;
+import net.minecraft.entity.EntityType;
+import net.minecraft.entity.SpawnGroup;
+import net.minecraft.util.Identifier;
+import net.minecraft.util.registry.Registry;
+import net.minecraft.world.biome.BiomeKeys;
+
+public class ModEntities {
+    public static final EntityType<ScorcherEntity> SCORCHER = Registry.register(
+            Registry.ENTITY_TYPE,
+            new Identifier("sakaplus", "scorcher"),
+            EntityType.Builder.create(ScorcherEntity::new, SpawnGroup.MONSTER)
+                    .setDimensions(EntityDimensions.fixed(0.6F, 1.8F))
+                    .fireImmune()
+                    .build()
+    );
+
+    public static void register() {
+        FabricDefaultAttributeRegistry.register(SCORCHER, ScorcherEntity.createAttributes());
+        BiomeModifications.addSpawn(
+                BiomeSelectors.includeByKey(BiomeKeys.CRIMSON_FOREST),
+                SpawnGroup.MONSTER,
+                SCORCHER,
+                20, 1, 2
+        );
+    }
+}
