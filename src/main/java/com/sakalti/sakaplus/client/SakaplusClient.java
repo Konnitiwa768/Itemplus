057917bbdda19203d3999dbfdd931fa9ddbe7a7f
Konnitiwa768 <konnitiwaa1@outlook.jp>
2025-06-09 17:26:19 +0900

    Create SakaplusClient.java

---
+++ src/main/java/com/sakalti/sakaplus/client/SakaplusClient.java
@@ -0,0 +1,11 @@
+import net.fabricmc.fabric.api.client.rendering.v1.EntityModelLayerRegistry;
+import com.sakalti.sakaplus.client.model.ScorcherModel;
+import com.sakalti.sakaplus.client.renderer.ScorcherRenderer;
+
+public class SakaplusClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+        EntityRendererRegistry.register(ModEntities.SCORCHER, ScorcherRenderer::new);
+        EntityModelLayerRegistry.registerModelLayer(ScorcherRenderer.MODEL_LAYER, ScorcherModel::getTexturedModelData);
+    }
+}
