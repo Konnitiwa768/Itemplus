1dde704661a1864e1e7ee6f1c9a82118e4f80878
Konnitiwa768 <konnitiwaa1@outlook.jp>
2025-06-11 06:14:36 +0900

    Update NyankoRenderer.java

--- src/main/java/com/sakalti/sakaplus/client/renderer/NyankoRenderer.java
+++ src/main/java/com/sakalti/sakaplus/client/renderer/NyankoRenderer.java
@@ -1,17 +1,20 @@
 package com.sakalti.sakaplus.client.renderer;
 
-import com.sakalti.sakaplus.registry.ModEntities;
-import com.sakalti.sakaplus.client.model.NyankoModel;
 import com.sakalti.sakaplus.entity.NyankoEntity;
+import com.sakalti.sakaplus.client.model.NyankoModel;
 import net.minecraft.client.render.entity.EntityRendererFactory;
 import net.minecraft.client.render.entity.MobEntityRenderer;
 import net.minecraft.util.Identifier;
+import net.minecraft.client.render.entity.model.EntityModelLayer;
 
 public class NyankoRenderer extends MobEntityRenderer<NyankoEntity, NyankoModel> {
     private static final Identifier TEXTURE = new Identifier("sakaplus", "textures/entity/nyanko.png");
 
+    public static final EntityModelLayer MODEL_LAYER = new EntityModelLayer(
+        new Identifier("sakaplus", "nyanko"), "main");
+
     public NyankoRenderer(EntityRendererFactory.Context context) {
-        super(context, new NyankoModel(context.getPart(ModEntities.NYANKO_LAYER)), 0.5f);
+        super(context, new NyankoModel(context.getPart(MODEL_LAYER)), 0.5f);
     }
 
     @Override
