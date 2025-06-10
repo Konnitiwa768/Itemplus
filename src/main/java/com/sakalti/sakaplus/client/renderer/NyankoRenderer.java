d217d7dc0791c74275f8ddb166e89ecc6f083940
Konnitiwa768 <konnitiwaa1@outlook.jp>
2025-06-11 05:44:44 +0900

    Create NyankoRenderer.java

---
+++ src/main/java/com/sakalti/sakaplus/client/renderer/NyankoRenderer.java
@@ -0,0 +1,21 @@
+package com.sakalti.sakaplus.client.renderer;
+
+import com.sakalti.sakaplus.ModEntities;
+import com.sakalti.sakaplus.client.model.NyankoModel;
+import com.sakalti.sakaplus.entity.NyankoEntity;
+import net.minecraft.client.render.entity.EntityRendererFactory;
+import net.minecraft.client.render.entity.MobEntityRenderer;
+import net.minecraft.util.Identifier;
+
+public class NyankoRenderer extends MobEntityRenderer<NyankoEntity, NyankoModel> {
+    private static final Identifier TEXTURE = new Identifier("sakaplus", "textures/entity/nyanko.png");
+
+    public NyankoRenderer(EntityRendererFactory.Context context) {
+        super(context, new NyankoModel(context.getPart(ModEntities.NYANKO_LAYER)), 0.5f);
+    }
+
+    @Override
+    public Identifier getTexture(NyankoEntity entity) {
+        return TEXTURE;
+    }
+}
