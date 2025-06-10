8aa3c696268d767120795ad1b9d06a3dfbd9a374
Konnitiwa768 <konnitiwaa1@outlook.jp>
2025-06-10 16:27:39 +0900

    Update OganesonRenderer.java

--- src/main/java/com/sakalti/sakaplus/client/renderer/OganesonRenderer.java
+++ src/main/java/com/sakalti/sakaplus/client/renderer/OganesonRenderer.java
@@ -1,20 +1,20 @@
 package com.sakalti.sakaplus.client.renderer;
 
 import com.sakalti.sakaplus.entity.OganesonEntity;
-import com.sakalti.sakaplus.client.model.ScorcherModel;
+import com.sakalti.sakaplus.client.model.OganesonModel;
 import net.minecraft.client.render.entity.EntityRendererFactory;
 import net.minecraft.client.render.entity.MobEntityRenderer;
 import net.minecraft.util.Identifier;
 import net.minecraft.client.render.entity.model.EntityModelLayer;
 
-public class OganesonRenderer extends MobEntityRenderer<OganesonEntity, ScorcherModel> {
+public class OganesonRenderer extends MobEntityRenderer<OganesonEntity, OganesonModel> {
     private static final Identifier TEXTURE = new Identifier("sakaplus", "textures/entity/oganeson.png");
 
     public static final EntityModelLayer MODEL_LAYER = new EntityModelLayer(
         new Identifier("sakaplus", "oganeson"), "main");
 
     public OganesonRenderer(EntityRendererFactory.Context context) {
-        super(context, new ScorcherModel(context.getPart(MODEL_LAYER)), 0.5f);
+        super(context, new OganesonModel(context.getPart(MODEL_LAYER)), 0.5f);
     }
 
     @Override
