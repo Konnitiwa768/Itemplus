dd7a68f6ab1f56ee76da2ec7c493bec73e482c01
Konnitiwa768 <konnitiwaa1@outlook.jp>
2025-06-11 05:29:08 +0900

    Create NyankoEntity.java

---
+++ src/main/java/com/sakalti/sakaplus/entity/NyankoEntity.java
@@ -0,0 +1,42 @@
+package com.sakalti.sakaplus.entity;
+
+import net.minecraft.entity.EntityType;
+import net.minecraft.entity.LivingEntity;
+import net.minecraft.entity.ai.goal.*;
+import net.minecraft.entity.attribute.DefaultAttributeContainer;
+import net.minecraft.entity.attribute.EntityAttributes;
+import net.minecraft.entity.damage.DamageSource;
+import net.minecraft.entity.mob.HostileEntity;
+import net.minecraft.entity.mob.MobEntity;
+import net.minecraft.entity.mob.PathAwareEntity;
+import net.minecraft.world.World;
+
+public class NyankoEntity extends HostileEntity {
+    public NyankoEntity(EntityType<? extends HostileEntity> entityType, World world) {
+        super(entityType, world);
+    }
+
+    @Override
+    protected void initGoals() {
+        this.goalSelector.add(1, new MeleeAttackGoal(this, 0.8, false)); // 攻撃速度
+        this.goalSelector.add(2, new WanderAroundFarGoal(this, 0.8));
+        this.goalSelector.add(3, new LookAtEntityGoal(this, LivingEntity.class, 8.0F));
+        this.goalSelector.add(4, new LookAroundGoal(this));
+        this.targetSelector.add(1, new ActiveTargetGoal<>(this, LivingEntity.class, true));
+    }
+
+    public static DefaultAttributeContainer.Builder createNyankoAttributes() {
+        return MobEntity.createMobAttributes()
+                .add(EntityAttributes.GENERIC_MAX_HEALTH, 17.0)
+                .add(EntityAttributes.GENERIC_MOVEMENT_SPEED, 0.25)
+                .add(EntityAttributes.GENERIC_ARMOR, 1.0)
+                .add(EntityAttributes.GENERIC_FOLLOW_RANGE, 25.0)
+                .add(EntityAttributes.GENERIC_ATTACK_DAMAGE, 6.0)
+                .add(EntityAttributes.GENERIC_ATTACK_SPEED, 0.8); // 1.25秒ごと（=0.8回/秒）
+    }
+
+    @Override
+    public boolean tryAttack(Entity target) {
+        return super.tryAttack(target);
+    }
+}
