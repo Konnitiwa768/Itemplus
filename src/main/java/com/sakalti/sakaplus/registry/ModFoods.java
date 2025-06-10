cefb25036ffd5a9372d32845e0b59b808b7f42e8
Konnitiwa768 <konnitiwaa1@outlook.jp>
2025-06-10 09:58:55 +0900

    Create ModFoods.java

---
+++ src/main/java/com/sakalti/sakaplus/registry/ModFoods.java
@@ -0,0 +1,8 @@
+package com.sakalti.sakaplus.registry;
+
+import net.minecraft.item.FoodComponent;
+
+public class ModFoods {
+    public static final FoodComponent GLOW_BERRY_BREAD = new FoodComponent.Builder().hunger(13).saturationModifier(0.9f).build();
+    public static final FoodComponent CHOCOLATE_BAR = new FoodComponent.Builder().hunger(8).saturationModifier(0.6f).build();
+}
