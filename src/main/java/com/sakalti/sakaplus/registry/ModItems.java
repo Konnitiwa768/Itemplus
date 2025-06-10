c90c12e3212dc402b543db8be6633b4ed64a8716
Konnitiwa768 <konnitiwaa1@outlook.jp>
2025-06-10 09:57:53 +0900

    Create ModItems.java

---
+++ src/main/java/com/sakalti/sakaplus/registry/ModItems.java
@@ -0,0 +1,16 @@
+package com.sakalti.sakaplus.registry;
+
+import com.sakalti.sakaplus.SakaPlusMod;
+import net.minecraft.item.Item;
+import net.minecraft.util.Identifier;
+import net.minecraft.util.registry.Registry;
+
+public class ModItems {
+    public static final Item GLOW_BERRY_BREAD = new Item(new Item.Settings().food(ModFoods.GLOW_BERRY_BREAD));
+    public static final Item CHOCOLATE_BAR = new Item(new Item.Settings().food(ModFoods.CHOCOLATE_BAR));
+
+    public static void register() {
+        Registry.register(Registry.ITEM, new Identifier(SakaPlusMod.MOD_ID, "glow_berry_bread"), GLOW_BERRY_BREAD);
+        Registry.register(Registry.ITEM, new Identifier(SakaPlusMod.MOD_ID, "chocolate_bar"), CHOCOLATE_BAR);
+    }
+}
