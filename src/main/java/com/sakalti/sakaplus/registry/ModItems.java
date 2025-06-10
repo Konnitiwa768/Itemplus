ec92a776115a56983ba329778aadd9533574bf73
Konnitiwa768 <konnitiwaa1@outlook.jp>
2025-06-10 19:37:52 +0900

    Update ModItems.java

--- src/main/java/com/sakalti/sakaplus/registry/ModItems.java
+++ src/main/java/com/sakalti/sakaplus/registry/ModItems.java
@@ -7,9 +7,13 @@ import net.minecraft.util.registry.Registry;
 public class ModItems {
     public static final Item GLOW_BERRY_BREAD = new Item(new Item.Settings().food(ModFoods.GLOW_BERRY_BREAD));
     public static final Item CHOCOLATE_BAR = new Item(new Item.Settings().food(ModFoods.CHOCOLATE_BAR));
+    // ここから追加
+    public static final Item SWEET_BERRY_PIE = new Item(new Item.Settings().food(ModFoods.SWEET_BERRY_PIE));
 
     public static void register() {
         Registry.register(Registry.ITEM, new Identifier("sakaplus", "glow_berry_bread"), GLOW_BERRY_BREAD);
         Registry.register(Registry.ITEM, new Identifier("sakaplus", "chocolate_bar"), CHOCOLATE_BAR);
+        // ここから追加
+        Registry.register(Registry.ITEM, new Identifier("sakaplus", "sweet_berry_pie"), SWEET_BERRY_PIE);
     }
 }
