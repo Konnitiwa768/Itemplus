c47708c99cfefe09fcb6ab1ca37eb5ff3e89700d
Konnitiwa768 <konnitiwaa1@outlook.jp>
2025-06-11 05:46:24 +0900

    Update SakaplusClient.java

--- src/main/java/com/sakalti/sakaplus/client/SakaplusClient.java
+++ src/main/java/com/sakalti/sakaplus/client/SakaplusClient.java
@@ -2,9 +2,12 @@ import net.fabricmc.fabric.api.client.rendering.v1.EntityModelLayerRegistry;
 import net.fabricmc.fabric.api.client.rendering.v1.EntityRendererRegistry;
 import com.sakalti.sakaplus.client.model.ScorcherModel;
 import com.sakalti.sakaplus.client.model.OganesonModel;
+import com.sakalti.sakaplus.client.model.NyankoModel;
 import com.sakalti.sakaplus.registry.ModEntities;
 import com.sakalti.sakaplus.entity.ScorcherEntity;
 import com.sakalti.sakaplus.client.renderer.ScorcherRenderer;
+import com.sakalti.sakaplus.entity.NyankoEntity;
+import com.sakalti.sakaplus.client.renderer.NyankoRenderer;
 import com.sakalti.sakaplus.entity.OganesonEntity;
 import com.sakalti.sakaplus.client.renderer.OganesonRenderer;
 import net.fabricmc.api.ClientModInitializer;
@@ -13,8 +16,10 @@ public class SakaplusClient implements ClientModInitializer {
     @Override
     public void onInitializeClient() {
         EntityRendererRegistry.register(ModEntities.SCORCHER, ScorcherRenderer::new);
+        EntityRendererRegistry.register(ModEntities.NYANKO, NyankoRenderer::new);
         EntityRendererRegistry.register(ModEntities.OGANESON, OganesonRenderer::new);
         EntityModelLayerRegistry.registerModelLayer(ScorcherRenderer.MODEL_LAYER, ScorcherModel::getTexturedModelData);
+        EntityModelLayerRegistry.registerModelLayer(NyankoRenderer.MODEL_LAYER, NyankoModel::getTexturedModelData);
         EntityModelLayerRegistry.registerModelLayer(OganesonRenderer.MODEL_LAYER, OganesonModel::getTexturedModelData);
     }
 }
