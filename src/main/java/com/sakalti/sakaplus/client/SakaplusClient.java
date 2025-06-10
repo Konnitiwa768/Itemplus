5771eab28e4e79b0119d989c2ee8aad423d03018
Konnitiwa768 <konnitiwaa1@outlook.jp>
2025-06-10 15:54:15 +0900

    Update SakaplusClient.java

--- src/main/java/com/sakalti/sakaplus/client/SakaplusClient.java
+++ src/main/java/com/sakalti/sakaplus/client/SakaplusClient.java
@@ -4,12 +4,15 @@ import com.sakalti.sakaplus.client.model.ScorcherModel;
 import com.sakalti.sakaplus.registry.ModEntities;
 import com.sakalti.sakaplus.entity.ScorcherEntity;
 import com.sakalti.sakaplus.client.renderer.ScorcherRenderer;
+import com.sakalti.sakaplus.entity.OganesonEntity;
+import com.sakalti.sakaplus.client.renderer.OganesonRenderer;
 import net.fabricmc.api.ClientModInitializer;
 
 public class SakaplusClient implements ClientModInitializer {
     @Override
     public void onInitializeClient() {
         EntityRendererRegistry.register(ModEntities.SCORCHER, ScorcherRenderer::new);
+        EntityRendererRegistry.register(ModEntities.OGANESON, OganesonRenderer::new);
         EntityModelLayerRegistry.registerModelLayer(ScorcherRenderer.MODEL_LAYER, ScorcherModel::getTexturedModelData);
     }
 }
