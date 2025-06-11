942998cd374dc638d9486c509b702d4e3e833288
Konnitiwa768 <konnitiwaa1@outlook.jp>
2025-06-11 15:35:26 +0900

    Create LightBlockEffect.java

---
+++ src/main/java/com/sakalti/sakaplus/effect/LightBlockEffect.java
@@ -0,0 +1,21 @@
+package com.sakalti.sakaplus.effect;
+
+import net.minecraft.entity.effect.StatusEffect;
+import net.minecraft.entity.effect.StatusEffectCategory;
+import net.minecraft.entity.LivingEntity;
+
+public class LightBlockEffect extends StatusEffect {
+    public LightBlockEffect() {
+        super(StatusEffectCategory.HARMFUL, 0xFFFFFF); // 白色
+    }
+
+    @Override
+    public void applyUpdateEffect(LivingEntity entity, int amplifier) {
+        // ここでは特別なtick処理は不要
+    }
+
+    @Override
+    public boolean canApplyUpdateEffect(int duration, int amplifier) {
+        return false;
+    }
+}
