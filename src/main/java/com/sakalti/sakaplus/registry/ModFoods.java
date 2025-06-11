f46610caaf6f6d6be27bd083139b3f3587a48d83
Konnitiwa768 <konnitiwaa1@outlook.jp>
2025-06-11 19:57:26 +0900

    Update ModFoods.java

--- src/main/java/com/sakalti/sakaplus/registry/ModFoods.java
+++ src/main/java/com/sakalti/sakaplus/registry/ModFoods.java
@@ -5,17 +5,17 @@ import net.minecraft.entity.effect.StatusEffectInstance;
 import net.minecraft.entity.effect.StatusEffects;
 
 public class ModFoods {
-    public static final FoodComponent GLOW_BERRY_BREAD = new FoodComponent.Builder().hunger(13).saturationModifier(0.9f).build();
-    public static final FoodComponent CHOCOLATE_BAR = new FoodComponent.Builder().hunger(8).saturationModifier(0.6f).build();
+    public static final FoodComponent GLOW_BERRY_BREAD = new FoodComponent.Builder().hunger(13).saturationModifier(3.9f).build();
+    public static final FoodComponent CHOCOLATE_BAR = new FoodComponent.Builder().hunger(8).saturationModifier(2.6f).build();
     // ここから追加
     public static final FoodComponent SWEET_BERRY_PIE = new FoodComponent.Builder()
         .hunger(7) // コスパ良く
-        .saturationModifier(0.8f)
+        .saturationModifier(4.5f)
         .statusEffect(new StatusEffectInstance(StatusEffects.REGENERATION, 60, 1), 1.0f) // 3秒=60tick, 再生II
         .build();
     public static final FoodComponent GLOW_BERRY_DONUT = new FoodComponent.Builder()
         .hunger(5) // コスパ良く
-        .saturationModifier(1.0f)
+        .saturationModifier(4.4f)
         .statusEffect(new StatusEffectInstance(StatusEffects.RESISTANCE, 110, 1), 1.0f) // 5.5秒=110tick, 耐性II
         .build();
 }
