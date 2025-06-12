37786346e00caa3f8fd7aa90324addfb0d38bbf5
Konnitiwa768 <konnitiwaa1@outlook.jp>
2025-06-13 08:31:27 +0900

    Update SakaplusMod2.java

--- src/main/java/com/sakalti/sakaplus/SakaplusMod2.java
+++ src/main/java/com/sakalti/sakaplus/SakaplusMod2.java
@@ -3,11 +3,13 @@ package com.sakalti.sakaplus;
 import net.fabricmc.api.ModInitializer;
 import com.sakalti.sakaplus.registry.ModEntities;
 import com.sakalti.sakaplus.block.ModBlocks2;
+import com.sakalti.sakaplus.registry.ModItems;
 
 public class SakaplusMod2 implements ModInitializer {
     @Override
     public void onInitialize() {
         ModBlocks2.registerBlocks();
         ModEntities.register();
+        ModItems.register();
     }
 }
