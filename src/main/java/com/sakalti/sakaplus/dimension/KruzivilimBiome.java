f86d6c6bb2b2b902d60f31405e4982a57bd288aa
Konnitiwa768 <konnitiwaa1@outlook.jp>
2025-06-16 06:22:17 +0900

    Update KruzivilimBiome.java

--- src/main/java/com/sakalti/sakaplus/dimension/KruzivilimBiome.java
+++ src/main/java/com/sakalti/sakaplus/dimension/KruzivilimBiome.java
@@ -22,8 +22,9 @@ public class KruzivilimBiome {
 
         // パッシブモブだけスポーン（例：牛・羊）
         spawnBuilder.spawn(SpawnGroup.CREATURE, new SpawnSettings.SpawnEntry(EntityType.COW, 10, 2, 4));
+        spawnBuilder.spawn(SpawnGroup.CREATURE, new SpawnSettings.SpawnEntry(ModEntities.KULOOGIE, 1, 1, 3));
         spawnBuilder.spawn(SpawnGroup.CREATURE, new SpawnSettings.SpawnEntry(ModEntities.KUR_BRUTE, 25, 2, 6));
-        spawnBuilder.spawn(SpawnGroup.MONSTER, new SpawnSettings.SpawnEntry(ModEntities.BATTLE_NYANKO, 3, 2, 5));
+        spawnBuilder.spawn(SpawnGroup.MONSTER, new SpawnSettings.SpawnEntry(ModEntities.BATTLE_NYANKO, 3, 1, 3));
         spawnBuilder.spawn(SpawnGroup.CREATURE, new SpawnSettings.SpawnEntry(EntityType.SHEEP, 10, 2, 4));
 
         // 敵モブなどはスポーンさせない
