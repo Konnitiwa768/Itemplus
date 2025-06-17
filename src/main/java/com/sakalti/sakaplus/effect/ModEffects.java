03db2e7931f23b268f3426d53d672fcac91a756f
Konnitiwa768 <konnitiwaa1@outlook.jp>
2025-06-18 06:43:10 +0900

    Update ModEffects.java

--- src/main/java/com/sakalti/sakaplus/effect/ModEffects.java
+++ src/main/java/com/sakalti/sakaplus/effect/ModEffects.java
@@ -7,9 +7,11 @@ import net.minecraft.util.registry.Registry;
 public class ModEffects {
     public static final StatusEffect LIGHT_BLOCK = new LightBlockEffect();
     public static final StatusEffect PARALYZE = new ParalyzeEffect();
+    public static final StatusEffect MUDO = new MudoStatusEffect();
 
     public static void registerEffects() {
         Registry.register(Registry.STATUS_EFFECT, new Identifier("sakaplus", "light_block"), LIGHT_BLOCK);
         Registry.register(Registry.STATUS_EFFECT, new Identifier("sakaplus", "paralyze"), PARALYZE);
+        Registry.register(Registry.STATUS_EFFECT, new Identifier("sakaplus", "mudo"), MUDO);
     }
 }
