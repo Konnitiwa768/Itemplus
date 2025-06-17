d0e445f8badab8f7f68be1ecdc286faf4d57ddc3
Konnitiwa768 <konnitiwaa1@outlook.jp>
2025-06-18 06:22:52 +0900

    Update and rename MW.java to ModWeapons.java

--- src/main/java/com/sakalti/sakaplus/registry/ModWeapons.java
+++ src/main/java/com/sakalti/sakaplus/registry/ModWeapons.java
@@ -5,7 +5,7 @@ import net.minecraft.item.Item;
 import net.minecraft.util.Identifier;
 import net.minecraft.util.registry.Registry;
 
-public class MW {
+public class ModWeapons {
     public static final Item LIGHTNING_STINGER = new LightningStinger();
 
     public static void registerItems() {
