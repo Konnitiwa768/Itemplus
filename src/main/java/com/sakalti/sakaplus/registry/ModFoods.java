1fa9832d1127365d6f9ebf61463786f980e9e863
Konnitiwa768 <konnitiwaa1@outlook.jp>
2025-06-20 17:46:30 +0900

    Update ModFoods.java

--- src/main/java/com/sakalti/sakaplus/registry/ModFoods.java
+++ src/main/java/com/sakalti/sakaplus/registry/ModFoods.java
@@ -1,32 +1,48 @@
 package com.sakalti.sakaplus.registry;
 
 import net.minecraft.item.FoodComponent;
-import com.sakalti.sakaplus.effect.ModEffects;
 import net.minecraft.entity.effect.StatusEffectInstance;
 import net.minecraft.entity.effect.StatusEffects;
+import com.sakalti.sakaplus.effect.ModEffects;
 
 public class ModFoods {
-    public static final FoodComponent GLOW_BERRY_BREAD = new FoodComponent.Builder().hunger(13).saturationModifier(3.9f).build();
-    public static final FoodComponent CHOCOLATE_BAR = new FoodComponent.Builder().hunger(8).saturationModifier(2.6f).build();
-    // ここから追加
+    public static final FoodComponent GLOW_BERRY_BREAD = new FoodComponent.Builder()
+        .hunger(13)
+        .saturationModifier(3.9f)
+        .build();
+
+    public static final FoodComponent CHOCOLATE_BAR = new FoodComponent.Builder()
+        .hunger(8)
+        .saturationModifier(2.6f)
+        .build();
+
     public static final FoodComponent SWEET_BERRY_PIE = new FoodComponent.Builder()
-        .hunger(7) // コスパ良く
+        .hunger(7)
         .saturationModifier(4.5f)
-        .statusEffect(new StatusEffectInstance(StatusEffects.REGENERATION, 60, 1), 1.0f) // 3秒=60tick, 再生II
+        .statusEffect(new StatusEffectInstance(StatusEffects.REGENERATION, 60, 1), 1.0f)
         .build();
+
     public static final FoodComponent KUR_BRUTE_MEAT = new FoodComponent.Builder()
-        .hunger(13) // コスパ良く
+        .hunger(13)
         .saturationModifier(15.5f)
-        .statusEffect(new StatusEffectInstance(ModEffects.LIGHT_BLOCK, 40, 3), 1.0f) // 3秒=60tick, 再生II
+        .statusEffect(new StatusEffectInstance(ModEffects.LIGHT_BLOCK, 40, 3), 1.0f)
         .build();
+
     public static final FoodComponent LIGHT_BERRY = new FoodComponent.Builder()
-        .hunger(4) // コスパ良く
+        .hunger(4)
         .saturationModifier(4.5f)
-        .statusEffect(new StatusEffectInstance(StatusEffects.HASTE, 400, 4), 1.0f) // 3秒=60tick, 再生II
+        .statusEffect(new StatusEffectInstance(StatusEffects.HASTE, 400, 4), 1.0f)
         .build();
+
     public static final FoodComponent GLOW_BERRY_DONUT = new FoodComponent.Builder()
-        .hunger(5) // コスパ良く
+        .hunger(5)
         .saturationModifier(4.4f)
-        .statusEffect(new StatusEffectInstance(StatusEffects.RESISTANCE, 110, 1), 1.0f) // 5.5秒=110tick, 耐性II
+        .statusEffect(new StatusEffectInstance(StatusEffects.RESISTANCE, 110, 1), 1.0f)
+        .build();
+
+    public static final FoodComponent BLOOMSQUER = new FoodComponent.Builder()
+        .hunger(6)
+        .saturationModifier(10.0f)
+        .statusEffect(new StatusEffectInstance(StatusEffects.RESISTANCE, 10, 4), 1.0f)
         .build();
 }
