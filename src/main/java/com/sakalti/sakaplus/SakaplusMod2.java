a7be835bf28704cd4e8a30e280d73ce3fcdafbd2
Konnitiwa768 <konnitiwaa1@outlook.jp>
2025-07-02 15:00:59 +0900

    Update SakaplusMod2.java

--- src/main/java/com/sakalti/sakaplus/SakaplusMod2.java
+++ src/main/java/com/sakalti/sakaplus/SakaplusMod2.java
@@ -4,7 +4,6 @@ import net.fabricmc.api.ModInitializer;
 import net.fabricmc.fabric.api.command.v2.CommandRegistrationCallback;
 
 import com.sakalti.sakaplus.tinco.item.ModItems5;
-import com.sakalti.sakaplus.tinco.client.AssembledToolBakedModel;
 import com.sakalti.sakaplus.command.KruzivilimCommand;
 import com.sakalti.sakaplus.registry.ModEntities333;
 import com.sakalti.sakaplus.registry.Weapons;
@@ -16,12 +15,6 @@ public class SakaplusMod2 implements ModInitializer {
     @Override
     public void onInitialize() {
         ModBlocks2.registerBlocks();
-        ModelLoadingRegistry.INSTANCE.registerResourceProvider(manager -> (resourceId, context) -> {
-            if (resourceId.getNamespace().equals("tinco") && resourceId.getPath().equals("assembled_tool")) {
-            return new AssembledToolBakedModel();
-            }
-          return null;
-        });
         Weapons.registerItems();
         ModEntities333.register();
         ModItems.register();
