3ea03bf19ce7baad67839ccd229a60da1fe2c9d9
Konnitiwa768 <konnitiwaa1@outlook.jp>
2025-07-02 14:44:52 +0900

    Update ToolTextureGenerator.java

--- src/main/java/com/sakalti/sakaplus/tinco/client/ToolTextureGenerator.java
+++ src/main/java/com/sakalti/sakaplus/tinco/client/ToolTextureGenerator.java
@@ -9,18 +9,14 @@ import java.io.IOException;
 
 public class ToolTextureGenerator {
 
-    public static DynamicTexture generateToolTexture(String head, String handle, String extra) {
+    public static DynamicTexture generate(String head, String handle, String extra) {
         try {
-            ResourceLocation headTex = new ResourceLocation("tinco:textures/item/" + head.toLowerCase() + "_head.png");
-            ResourceLocation handleTex = new ResourceLocation("tinco:textures/item/" + handle.toLowerCase() + "_handle.png");
-            ResourceLocation extraTex = new ResourceLocation("tinco:textures/item/" + extra.toLowerCase() + "_extra.png");
+            NativeImage base = load("item/" + handle + "_handle");
+            NativeImage headImg = load("item/" + head + "_head");
+            NativeImage extraImg = load("item/" + extra + "_extra");
 
-            NativeImage base = loadImage(handleTex);
-            NativeImage headImg = loadImage(headTex);
-            NativeImage extraImg = loadImage(extraTex);
-
-            overlayImage(base, headImg);
-            overlayImage(base, extraImg);
+            overlay(base, headImg);
+            overlay(base, extraImg);
 
             return new DynamicTexture(base);
 
@@ -30,17 +26,19 @@ public class ToolTextureGenerator {
         }
     }
 
-    private static NativeImage loadImage(ResourceLocation location) throws IOException {
-        return NativeImage.read(Minecraft.getInstance().getResourceManager().open(location));
+    private static NativeImage load(String path) throws IOException {
+        ResourceLocation res = new ResourceLocation("sakaplus", "textures/" + path + ".png");
+        return NativeImage.read(Minecraft.getInstance().getResourceManager().open(res));
     }
 
-    private static void overlayImage(NativeImage base, NativeImage overlay) {
-        for (int x = 0; x < base.getWidth(); x++) {
-            for (int y = 0; y < base.getHeight(); y++) {
-                int color = overlay.getPixelRGBA(x, y);
-                if ((color >> 24) != 0) { // 透明ピクセル無視
-                    base.setPixelRGBA(x, y, color);
-                }
+    private static void overlay(NativeImage base, NativeImage overlay) {
+        if (base.getWidth() != overlay.getWidth() || base.getHeight() != overlay.getHeight()) {
+            throw new IllegalStateException("Texture sizes differ!");
+        }
+        for (int y = 0; y < base.getHeight(); y++) {
+            for (int x = 0; x < base.getWidth(); x++) {
+                int col = overlay.getPixelRGBA(x, y);
+                if ((col >> 24 & 0xFF) > 0) base.setPixelRGBA(x, y, col);
             }
         }
     }
