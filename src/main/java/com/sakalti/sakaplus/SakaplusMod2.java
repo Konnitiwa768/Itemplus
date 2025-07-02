d38644b6ba1df885b375121ee845382175d8db75
Konnitiwa768 <konnitiwaa1@outlook.jp>
2025-07-02 14:04:16 +0900

    Update SakaplusMod2.java

--- src/main/java/com/sakalti/sakaplus/SakaplusMod2.java
+++ src/main/java/com/sakalti/sakaplus/SakaplusMod2.java
@@ -3,6 +3,7 @@ package com.sakalti.sakaplus;
 import net.fabricmc.api.ModInitializer;
 import net.fabricmc.fabric.api.command.v2.CommandRegistrationCallback;
 
+import com.sakalti.sakaplus.tinco.item.ModItems5;
 import com.sakalti.sakaplus.command.KruzivilimCommand;
 import com.sakalti.sakaplus.registry.ModEntities333;
 import com.sakalti.sakaplus.registry.Weapons;
@@ -18,6 +19,7 @@ public class SakaplusMod2 implements ModInitializer {
         ModEntities333.register();
         ModItems.register();
         SakaplusDimensions.register();
+        ModItems5.register();
         // コマンドの登録（Fabric API v2）
         CommandRegistrationCallback.EVENT.register((dispatcher, registryAccess, environment) -> {
             KruzivilimCommand.register(dispatcher);
