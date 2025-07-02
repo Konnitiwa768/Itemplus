78b353ee0d5f143b5ae3a111248ec5dd19849485
Konnitiwa768 <konnitiwaa1@outlook.jp>
2025-07-02 14:59:03 +0900

    Update ToolTextureGenerator.java

--- src/main/java/com/sakalti/sakaplus/tinco/client/ToolTextureGenerator.java
+++ src/main/java/com/sakalti/sakaplus/tinco/client/ToolTextureGenerator.java
@@ -6,20 +6,20 @@ import net.minecraft.client.renderer.texture.DynamicTexture;
 import net.minecraft.resources.ResourceLocation;
 
 import java.io.IOException;
+import java.util.Optional;
 
 public class ToolTextureGenerator {
 
     public static DynamicTexture generate(String head, String handle, String extra) {
         try {
-            NativeImage base = load("item/" + handle + "_handle");
-            NativeImage headImg = load("item/" + head + "_head");
-            NativeImage extraImg = load("item/" + extra + "_extra");
+            NativeImage base = load("item/" + handle.toLowerCase() + "_handle");
+            NativeImage headImg = load("item/" + head.toLowerCase() + "_head");
+            NativeImage extraImg = load("item/" + extra.toLowerCase() + "_extra");
 
             overlay(base, headImg);
             overlay(base, extraImg);
 
             return new DynamicTexture(base);
-
         } catch (IOException e) {
             e.printStackTrace();
             return null;
@@ -27,18 +27,22 @@ public class ToolTextureGenerator {
     }
 
     private static NativeImage load(String path) throws IOException {
-        ResourceLocation res = new ResourceLocation("sakaplus", "textures/" + path + ".png");
-        return NativeImage.read(Minecraft.getInstance().getResourceManager().open(res));
+        ResourceLocation res = new ResourceLocation("tinco", "textures/" + path + ".png");
+        Optional<NativeImage> image = NativeImage.readToOptional(Minecraft.getInstance().getResourceManager().open(res));
+        if (image.isPresent()) {
+            return image.get();
+        } else {
+            throw new IOException("Texture not found: " + res);
+        }
     }
 
     private static void overlay(NativeImage base, NativeImage overlay) {
-        if (base.getWidth() != overlay.getWidth() || base.getHeight() != overlay.getHeight()) {
-            throw new IllegalStateException("Texture sizes differ!");
-        }
         for (int y = 0; y < base.getHeight(); y++) {
             for (int x = 0; x < base.getWidth(); x++) {
-                int col = overlay.getPixelRGBA(x, y);
-                if ((col >> 24 & 0xFF) > 0) base.setPixelRGBA(x, y, col);
+                int color = overlay.getPixelRGBA(x, y);
+                if ((color >> 24 & 0xFF) > 0) {
+                    base.setPixelRGBA(x, y, color);
+                }
             }
         }
     }
