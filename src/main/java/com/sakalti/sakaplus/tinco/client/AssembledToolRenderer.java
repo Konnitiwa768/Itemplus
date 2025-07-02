96c42f92276a9d5bbab77724141d0b805e1390c2
Konnitiwa768 <konnitiwaa1@outlook.jp>
2025-07-02 15:46:50 +0900

    Update and rename AssembledToolRendrer.java to AssembledToolRenderer.java

---
+++ src/main/java/com/sakalti/sakaplus/tinco/client/AssembledToolRenderer.java
@@ -0,0 +1,34 @@
+package com.sakalti.sakaplus.tinco.client;
+
+import net.minecraft.world.item.ItemStack;
+
+public class AssembledToolRenderer {
+
+    public void debugPrint(ItemStack stack) {
+        var tag = stack.getOrCreateTag();
+        String head = tag.getString("head");
+        String handle = tag.getString("handle");
+        String extra = tag.getString("extra");
+
+        String headMat = parseMaterial(head);
+        String handleMat = parseMaterial(handle);
+        String extraMat = parseMaterial(extra);
+
+        int headColor = ToolColorGenerator.getColor(headMat);
+        int handleColor = ToolColorGenerator.getColor(handleMat);
+        int extraColor = ToolColorGenerator.getColor(extraMat);
+
+        System.out.println("Head: " + headMat + " Color: " + Integer.toHexString(headColor));
+        System.out.println("Handle: " + handleMat + " Color: " + Integer.toHexString(handleColor));
+        System.out.println("Extra: " + extraMat + " Color: " + Integer.toHexString(extraColor));
+    }
+
+    private String parseMaterial(String partStr) {
+        if (partStr == null) return "";
+        String lower = partStr.toLowerCase();
+        if (lower.endsWith("_handle")) return lower.substring(0, lower.length() - 7);
+        if (lower.endsWith("_head")) return lower.substring(0, lower.length() - 5);
+        if (lower.endsWith("_extra")) return lower.substring(0, lower.length() - 6);
+        return lower;
+    }
+}
