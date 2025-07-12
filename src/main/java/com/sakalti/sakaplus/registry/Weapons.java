c04f230623a0761728ace486350a8667403bc45f
Konnitiwa768 <konnitiwaa1@outlook.jp>
2025-07-12 11:29:15 +0900

    Update Weapons.java

--- src/main/java/com/sakalti/sakaplus/registry/Weapons.java
+++ src/main/java/com/sakalti/sakaplus/registry/Weapons.java
@@ -1,14 +1,15 @@
 package com.sakalti.sakaplus.registry;
 
-import com.sakalti.sakaplus.item.LightningStinger;
+import com.sakalti.sakaplus.item.*;
 import net.minecraft.item.Item;
 import net.minecraft.util.Identifier;
 import net.minecraft.util.registry.Registry;
 
 public class Weapons {
     public static final Item LIGHTNING_STINGER = new LightningStinger();
-
+    public static final Item LIGHTNING_SPARK = new LightningSpark();
     public static void registerItems() {
         Registry.register(Registry.ITEM, new Identifier("sakaplus", "lightning_stinger"), LIGHTNING_STINGER);
+        Registry.register(Registry.ITEM, new Identifier("sakaplus", "lightning_spark"), LIGHTNING_SPARK);
     }
 }
