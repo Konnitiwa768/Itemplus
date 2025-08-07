e22498762442533ce9b68f98a2aee11a1e1eb4b2
Konnitiwa768 <konnitiwaa1@outlook.jp>
2025-08-07 18:11:22 +0900

    Create LongtimePotionRegistry.java

---
+++ src/main/java/com/sakalti/sakaplus/registry/LongtimePotionRegistry.java
@@ -0,0 +1,30 @@
+// src/main/java/com/sakalti/sakaplus/registry/LongtimePotionRegistry.java
+package com.sakalti.sakaplus.registry;
+
+import com.sakalti.sakaplus.potion.LongtimePotionItem;
+import com.sakalti.sakaplus.effect.ModEffects;
+
+import net.minecraft.potion.Potion;
+import net.minecraft.potion.StatusEffect;
+import net.minecraft.potion.StatusEffectInstance;
+import net.minecraft.util.registry.Registries;
+import net.minecraft.util.registry.Registry;
+import net.minecraft.util.Identifier;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemGroup;
+
+public class LongtimePotionRegistry {
+    public static void registerAll() {
+        for (StatusEffect effect : ModEffects.ALL) {
+            int duration = 9999; // 効果時間2倍の例
+            Potion potion = new Potion(new StatusEffectInstance(effect, duration));
+            String name = effect.getTranslationKey().replace("effect.", "") + "_longtime";
+            Identifier id = new Identifier("sakaplus", name);
+
+            Registry.register(Registries.POTION, id, potion);
+            Registry.register(Registries.ITEM, id,
+                new LongtimePotionItem(new Item.Settings().maxCount(1).group(ItemGroup.BREWING))
+            );
+        }
+    }
+}
