17734f766e0bdb95ccda1e15c5d456146be900ed
Konnitiwa768 <konnitiwaa1@outlook.jp>
2025-08-07 19:33:48 +0900

    Update ParalyzeEffect.java

--- src/main/java/com/sakalti/sakaplus/effect/ParalyzeEffect.java
+++ src/main/java/com/sakalti/sakaplus/effect/ParalyzeEffect.java
@@ -5,7 +5,7 @@ import net.minecraft.entity.effect.StatusEffectCategory;
 import net.minecraft.entity.LivingEntity;
 import net.minecraft.entity.ai.goal.GoalSelector;
 
-public class ParalyzeEffect extends StatusEffect {
+public class ParalyzeEffect extends StatusEffects {
     public ParalyzeEffect() {
         super(StatusEffectCategory.HARMFUL, 0xAAAAFF); // 色は適宜
     }
