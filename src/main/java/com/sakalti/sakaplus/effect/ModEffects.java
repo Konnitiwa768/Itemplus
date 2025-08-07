3394af90338c7c3fcbe734472c1be1f2a00ef9d7
Konnitiwa768 <konnitiwaa1@outlook.jp>
2025-08-07 18:48:45 +0900

    Update ModEffects.java

--- src/main/java/com/sakalti/sakaplus/effect/ModEffects.java
+++ src/main/java/com/sakalti/sakaplus/effect/ModEffects.java
@@ -9,8 +9,80 @@ public class ModEffects {
     public static final StatusEffect PARALYZE = new ParalyzeEffect();
     public static final StatusEffect MUDO = new MudoStatusEffect();
     public static final StatusEffect ADAPT = new AdaptEffect();
-    
-    public static final StatusEffect[] ALL = { MUDO, LIGHT_BLOCK, ADAPT, PARALYZE };
+
+    // バニラのエフェクト
+    public static final StatusEffect[] VANILLA = {
+        StatusEffects.SPEED,
+        StatusEffects.SLOWNESS,
+        StatusEffects.HASTE,
+        StatusEffects.MINING_FATIGUE,
+        StatusEffects.STRENGTH,
+        StatusEffects.INSTANT_HEALTH,
+        StatusEffects.INSTANT_DAMAGE,
+        StatusEffects.JUMP_BOOST,
+        StatusEffects.NAUSEA,
+        StatusEffects.REGENERATION,
+        StatusEffects.RESISTANCE,
+        StatusEffects.FIRE_RESISTANCE,
+        StatusEffects.WATER_BREATHING,
+        StatusEffects.INVISIBILITY,
+        StatusEffects.BLINDNESS,
+        StatusEffects.NIGHT_VISION,
+        StatusEffects.HUNGER,
+        StatusEffects.WEAKNESS,
+        StatusEffects.POISON,
+        StatusEffects.WITHER,
+        StatusEffects.HEALTH_BOOST,
+        StatusEffects.ABSORPTION,
+        StatusEffects.SATURATION,
+        StatusEffects.GLOWING,
+        StatusEffects.LEVITATION,
+        StatusEffects.LUCK,
+        StatusEffects.UNLUCK,
+        StatusEffects.SLOW_FALLING,
+        StatusEffects.CONDUIT_POWER,
+        StatusEffects.DOLPHINS_GRACE,
+        StatusEffects.BAD_OMEN,
+        StatusEffects.HERO_OF_THE_VILLAGE,
+        StatusEffects.DARKNESS
+    };
+
+    public static final StatusEffect[] ALL = {
+        MUDO, LIGHT_BLOCK, ADAPT, PARALYZE,
+        StatusEffects.SPEED,
+        StatusEffects.SLOWNESS,
+        StatusEffects.HASTE,
+        StatusEffects.MINING_FATIGUE,
+        StatusEffects.STRENGTH,
+        StatusEffects.INSTANT_HEALTH,
+        StatusEffects.INSTANT_DAMAGE,
+        StatusEffects.JUMP_BOOST,
+        StatusEffects.NAUSEA,
+        StatusEffects.REGENERATION,
+        StatusEffects.RESISTANCE,
+        StatusEffects.FIRE_RESISTANCE,
+        StatusEffects.WATER_BREATHING,
+        StatusEffects.INVISIBILITY,
+        StatusEffects.BLINDNESS,
+        StatusEffects.NIGHT_VISION,
+        StatusEffects.HUNGER,
+        StatusEffects.WEAKNESS,
+        StatusEffects.POISON,
+        StatusEffects.WITHER,
+        StatusEffects.HEALTH_BOOST,
+        StatusEffects.ABSORPTION,
+        StatusEffects.SATURATION,
+        StatusEffects.GLOWING,
+        StatusEffects.LEVITATION,
+        StatusEffects.LUCK,
+        StatusEffects.UNLUCK,
+        StatusEffects.SLOW_FALLING,
+        StatusEffects.CONDUIT_POWER,
+        StatusEffects.DOLPHINS_GRACE,
+        StatusEffects.BAD_OMEN,
+        StatusEffects.HERO_OF_THE_VILLAGE,
+        StatusEffects.DARKNESS
+    };
     public static void registerEffects() {
         Registry.register(Registry.STATUS_EFFECT, new Identifier("sakaplus", "light_block"), LIGHT_BLOCK);
         Registry.register(Registry.STATUS_EFFECT, new Identifier("sakaplus", "paralyze"), PARALYZE);
